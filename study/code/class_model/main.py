c965b39630046136d04f47b596ff22cadac80279
jh79783 <jh79783@naver.com>
2020-11-03 22:07:28 +0900

    mlp model class

---
+++ study/code/class_model/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import class_model.dataset_mode as dm
+import class_model.mlp_model as mm
+
+np.random.seed(1234)
+
+# def randomize():
+#     np.random.seed(time.time())
+
+
+def main():
+    data = dm.Select('flower', 'select')
+    model = mm.MlpModel("flower_model", data, [10])
+    model.exec_all(epoch_count=10, report=2)
+
+
+if __name__ == '__main__':
+    main()
+
+
